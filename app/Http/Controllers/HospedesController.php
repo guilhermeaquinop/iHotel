5f397d6ad50b19f0b66f67bd9440f0f7fc8246e1
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2022-10-18 23:46:18 -0300

    0001 - HospedesController: Desenvolvido método store

--- app/Http/Controllers/HospedesController.php
+++ app/Http/Controllers/HospedesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Hospedes;
 
 class HospedesController extends Controller
 {
@@ -12,6 +13,21 @@ class HospedesController extends Controller
     
     public function store(Request $request)
     {
-
+        $hospede = new Hospedes();
+        $hospede->nome = $request->nome;
+        $hospede->telefone = $request->telefone;
+        $hospede->profissao = $request->profissao;
+        $hospede->nacionalidade = $request->nacionalidade;
+        $hospede->data_nascimento = $request->data_nascimento;
+        $hospede->genero = $request->genero;
+        $hospede->tipo_documento = $request->tipo_documento;
+        $hospede->numero_documento = $request->numero_documento;
+        $hospede->cpf = $request->cpf;
+        $hospede->cidade = $request->cidade;
+        $hospede->uf = $request->uf;
+        $hospede->endereco = $request->endereco;
+        $hospede->email = $request->email;
+        $hospede->save();
+        return redirect('hospedes/cadastrar')->with('cadastroRealizado', 'Cadastro realizado com sucesso!');
     }
 }
