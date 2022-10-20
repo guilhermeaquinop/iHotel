546b7509fe288ac7d4883a01b39ab0a17e699358
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2022-10-19 22:40:22 -0300

    0002 - Criado layout para o sidebar lateral & exportado layout para a view main

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,108 @@
+<div class="logo-container">
+    <a href="/ihotel" class="d-flex aling-itens-center">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png"
+            alt="" class="d-inline-block align-text-top">
+        <span>iHotel</span>
+    </a>
+</div>
+
+<div class="accordion accordion-flush" id="accordionFlushExample">
+    <ul>
+        <li>
+            <div class="button-accordion" data-bs-toggle="collapse" data-bs-target="#hospedes"
+                aria-expanded="false" aria-controls="flush-collapseOne">
+                <span href="#" class="accordion-header" id="flush-headingOne">Hóspedes</span>
+                <i class="bi bi-chevron-down"></i>
+            </div>
+            <div id="hospedes" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+                data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">
+                    <ul>
+                        <li>
+                            <a href="/hospedes/cadastrar" class="accordion-item" id="flush-headingOne">
+                                <div class="button-accordion-item">
+                                    <i class="bi bi-person-plus"></i>
+                                    <span>Novo cadastro</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/hospedes/consultar" class="accordion-item" id="flush-headingOne">
+                                <div class="button-accordion-item">
+                                    <i class="bi bi-card-list"></i>
+                                    <span>Consultar cadastro</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </li>
+
+        <!--                 <li>
+            <div class="button-accordion" data-bs-toggle="collapse" data-bs-target="#hospedagem" aria-expanded="false" aria-controls="flush-collapseOne">
+                <span href="#" class="accordion-header" id="flush-headingOne">Hospedagem</span>
+                <i class="bi bi-chevron-down"></i>
+            </div>
+            <div id="hospedagem" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">
+                    <ul>
+                        <li>
+                            <a href="" class="accordion-item" id="flush-headingOne">
+                                <div class="button-accordion-item">
+                                    <i class="bi bi-person-plus"></i>
+                                    <span>Check-In</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" class="accordion-item" id="flush-headingOne">
+                                <div class="button-accordion-item">
+                                    <i class="bi bi-card-list"></i>
+                                    <span>Check-Out</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </li> -->
+
+        <!--                 <li>
+            <div class="button-accordion" data-bs-toggle="collapse" data-bs-target="#apartamentos" aria-expanded="false" aria-controls="flush-collapseOne">
+                <span href="#" class="accordion-header" id="flush-headingOne">Apartamentos</span>
+                <i class="bi bi-chevron-down"></i>
+            </div>
+            <div id="apartamentos" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">
+                    <ul>
+                        <li>
+                            <a href="/ihotel/quartos/painel" class="accordion-item" id="flush-headingOne">
+                                <div class="button-accordion-item">
+                                    <i class="bi bi-window"></i>
+                                    <span>Painel</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/ihotel/quartos/cadastrar" class="accordion-item" id="flush-headingOne">
+                                <div class="button-accordion-item">
+                                    <i class="bi bi-plus-lg"></i>
+                                    <span>Novo cadastro</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/ihotel/quartos/consultar" class="accordion-item" id="flush-headingOne">
+                                <div class="button-accordion-item">
+                                    <i class="bi bi-card-list"></i>
+                                    <span>Consultar cadastro</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </li> -->
+    </ul>
+</div>
