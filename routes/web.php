a89b54644f7892fc3e77bc7a16b44a8b021eae69
Guilherme de Aquino <guilhermeaqn_dev@outlook.com>
2022-10-19 22:22:52 -0300

    0002 - Nova rota: hospedes/consultar

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 Route::prefix('/hospedes')->group(function(){
     Route::get('/cadastrar', [HospedesController::class, 'create'])->name('hospedes.view-cadastrar');
     Route::post('/cadastrar', [HospedesController::class, 'store'])->name('hospedes.cadastrar');
+    Route::get('/consultar', [HospedesController::class, 'show'])->name('hospedes.consultar');
 });
 
 
